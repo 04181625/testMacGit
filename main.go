d666cad2ed0ffb3f1b7c76dda46510679bb7f4e1
04181625 <1115447418@qq.com>
2023-10-10 15:16:27 +0800

    https

--- main.go
+++ main.go
@@ -6,5 +6,7 @@ func main() {
 	fmt.Println("hello git add ssh")
 	fmt.Println("hello git add branch")
 	fmt.Println("hello git add https")
+	fmt.Println("hello git add https")
+	fmt.Println("hello git add https")
 
 }
